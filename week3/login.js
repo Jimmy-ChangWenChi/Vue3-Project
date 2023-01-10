22c60d31e164f7cc895b6c6a049bf517a3ed40bd
Jimmy ChangWenChi <geograp232@gmail.com>
2023-01-10 22:30:08 +0800

    add week 3

---
+++ week3/login.js
@@ -0,0 +1,31 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+
+createApp({
+    data(){
+        return {
+            user: {
+                username:"",
+                password:""
+            },
+            test:"test"
+        }
+    },
+    methods: {
+        login(){
+            const apiPath = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+
+            axios.post(apiPath,this.user)
+            .then((res) => {
+                console.log("success");
+                const { token,expired } = res.data;
+                console.log(token, expired);
+                document.cookie = `JimmyToken=${token};expired=${new Date(expired)}; `;
+                window.location = `upload_image.html`;
+            })
+            .catch((err)=>{
+                alert(err.response.data.message);
+            })
+        },
+    }
+}).mount("#app");
---
+++ week3/products.js
@@ -0,0 +1,18 @@
+import {createApp} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js";
+
+
+const app = {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    }
+}
+
+createApp(app).mount("#app");
